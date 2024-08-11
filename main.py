128c049fc2a9b28469d85befa77b1036f86a67cc
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2024-08-12 00:04:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,29 +12,29 @@ df = pd.read_excel('Assignment Data Analyst MSIB Batch 7.xlsx')
 df_facebook = df[df['ga:sourceMedium'] == 'facebook / cpc']
 df_google = df[df['ga:sourceMedium'] == 'google / cpc']
 
-st.header('Halaman dengan jumlah pengguna tertinggi')
-st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
-      'ga:users': 'sum'
-  }).sort_values(by='ga:users', ascending = False).head())
-
-st.header('Halaman dengan jumlah bounce rate tertinggi')
-st.table(df_facebook.groupby(by='ga:pageTitle').agg({
-    'ga:pageviews': 'sum'
-}).sort_values(by='ga:pageviews', ascending = False).head())
-
-st.header('Halaman dengan jumlah pembaca halaman tertinggi')
-st.table(df_facebook.groupby(by='ga:pageTitle').agg({
-    'ga:pageviews': 'sum'
-}).sort_values(by='ga:pageviews', ascending = False).head())
-
-st.header('Halaman dengan jumlah pembaca halaman per sesi tertinggi')
-st.table(df_facebook.groupby(by='ga:pageTitle').agg({
-    'ga:pageviewsPerSession': 'sum'
-}).sort_values(by='ga:pageviewsPerSession', ascending = False).head())
-
-st.header('Halaman dengan rata-rata waktu membaca halaman tertinggi')
-st.table(df_facebook.groupby(by='ga:pageTitle').agg({
-    'ga:avgTimeOnPage': 'sum'
-}).sort_values(by='ga:avgTimeOnPage', ascending = False).head())
-
-  
+tab1, tab2 = st.tabs(["Facebook", "Google"])
+with tab1:
+      st.header('Halaman dengan jumlah pengguna tertinggi')
+      st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
+            'ga:users': 'sum'
+        }).sort_values(by='ga:users', ascending = False).head())
+      
+      st.header('Halaman dengan jumlah bounce rate tertinggi')
+      st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+          'ga:pageviews': 'sum'
+      }).sort_values(by='ga:pageviews', ascending = False).head())
+      
+      st.header('Halaman dengan jumlah pembaca halaman tertinggi')
+      st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+          'ga:pageviews': 'sum'
+      }).sort_values(by='ga:pageviews', ascending = False).head())
+      
+      st.header('Halaman dengan jumlah pembaca halaman per sesi tertinggi')
+      st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+          'ga:pageviewsPerSession': 'sum'
+      }).sort_values(by='ga:pageviewsPerSession', ascending = False).head())
+      
+      st.header('Halaman dengan rata-rata waktu membaca halaman tertinggi')
+      st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+          'ga:avgTimeOnPage': 'sum'
+      }).sort_values(by='ga:avgTimeOnPage', ascending = False).head())
