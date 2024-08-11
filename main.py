aea3906fa482b1fe1d8e8011b7c1fd4ecc0059b8
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2024-08-12 00:02:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,29 +12,28 @@ df = pd.read_excel('Assignment Data Analyst MSIB Batch 7.xlsx')
 df_facebook = df[df['ga:sourceMedium'] == 'facebook / cpc']
 df_google = df[df['ga:sourceMedium'] == 'google / cpc']
 
-tab1, tab2, tab3 = st.tabs(["Peringkat Halaman", "Facebook", "Google"])
 st.header('Halaman dengan jumlah pengguna tertinggi')
-      st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
+st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
       'ga:users': 'sum'
   }).sort_values(by='ga:users', ascending = False).head())
-  with col2:
-    st.header('Halaman dengan jumlah bounce rate tertinggi')
-    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+
+st.header('Halaman dengan jumlah bounce rate tertinggi')
+st.table(df_facebook.groupby(by='ga:pageTitle').agg({
     'ga:pageviews': 'sum'
 }).sort_values(by='ga:pageviews', ascending = False).head())
-  with col3:
-    st.header('Halaman dengan jumlah pembaca halaman tertinggi')
-    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+
+st.header('Halaman dengan jumlah pembaca halaman tertinggi')
+st.table(df_facebook.groupby(by='ga:pageTitle').agg({
     'ga:pageviews': 'sum'
 }).sort_values(by='ga:pageviews', ascending = False).head())
-  with col4:
-    st.header('Halaman dengan jumlah pembaca halaman per sesi tertinggi')
-    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+
+st.header('Halaman dengan jumlah pembaca halaman per sesi tertinggi')
+st.table(df_facebook.groupby(by='ga:pageTitle').agg({
     'ga:pageviewsPerSession': 'sum'
 }).sort_values(by='ga:pageviewsPerSession', ascending = False).head())
-  with col5:
-    st.header('Halaman dengan rata-rata waktu membaca halaman tertinggi')
-    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+
+st.header('Halaman dengan rata-rata waktu membaca halaman tertinggi')
+st.table(df_facebook.groupby(by='ga:pageTitle').agg({
     'ga:avgTimeOnPage': 'sum'
 }).sort_values(by='ga:avgTimeOnPage', ascending = False).head())
 
