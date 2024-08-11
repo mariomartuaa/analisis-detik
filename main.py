7246fcd2ad97666af194ac48f610164067949636
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2024-08-11 23:26:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ st.title('Bike Sharing Dataset Dashboard')
 st.markdown('- Python libraries: numpy, pandas, streamlit, matplotlib, seaborn')
 st.markdown('- Data source: https://docs.google.com/spreadsheets/d/1HoTUoAWX3jdS3WPz6chBwiEzj7BHD4uSd5Flx3bsbWE/edit?usp=sharing')
 
-df = pd.read_csv('Assignment Data Analyst MSIB Batch 7.csv')
+df = pd.read_excel('Assignment Data Analyst MSIB Batch 7.xlsx')
 
 tab1, tab2, tab3 = st.tabs(["Dataset Table", "Counting Hourly", " Counting Time Span"])
