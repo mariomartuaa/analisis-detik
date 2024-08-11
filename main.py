2f4095a655fd4504939c9eb903b1507bd9d97139
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2024-08-12 00:01:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,29 @@ df_facebook = df[df['ga:sourceMedium'] == 'facebook / cpc']
 df_google = df[df['ga:sourceMedium'] == 'google / cpc']
 
 tab1, tab2, tab3 = st.tabs(["Peringkat Halaman", "Facebook", "Google"])
-
-with tab1:
-  option = st.selectbox(
-    ("ga:users", "ga:bounceRate", "ga:pageviews", "ga:pageviewsPerSession", "ga:avgTimeOnPage"),
-    placeholder="Select contact method..."
-  )
-  col1, col2 = st.columns(2)
-  with col1:
-    st.header('Facebook')
-    st.table(df_facebook.groupby(by=option).agg({
-    option: 'sum'
-}).sort_values(by=option, ascending = False).head())
+st.header('Halaman dengan jumlah pengguna tertinggi')
+      st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
+      'ga:users': 'sum'
+  }).sort_values(by='ga:users', ascending = False).head())
   with col2:
-    st.header('Google')
-    st.table(df_google.groupby(by=option).agg({
-    option: 'sum'
-}).sort_values(by=option, ascending = False).head())
+    st.header('Halaman dengan jumlah bounce rate tertinggi')
+    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+    'ga:pageviews': 'sum'
+}).sort_values(by='ga:pageviews', ascending = False).head())
+  with col3:
+    st.header('Halaman dengan jumlah pembaca halaman tertinggi')
+    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+    'ga:pageviews': 'sum'
+}).sort_values(by='ga:pageviews', ascending = False).head())
+  with col4:
+    st.header('Halaman dengan jumlah pembaca halaman per sesi tertinggi')
+    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+    'ga:pageviewsPerSession': 'sum'
+}).sort_values(by='ga:pageviewsPerSession', ascending = False).head())
+  with col5:
+    st.header('Halaman dengan rata-rata waktu membaca halaman tertinggi')
+    st.table(df_facebook.groupby(by='ga:pageTitle').agg({
+    'ga:avgTimeOnPage': 'sum'
+}).sort_values(by='ga:avgTimeOnPage', ascending = False).head())
+
   
