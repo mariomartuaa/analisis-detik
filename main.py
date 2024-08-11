46a2b4aa037d523e3142dfceebde82865c72edce
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2024-08-11 23:36:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ st.markdown('- Python libraries: numpy, pandas, streamlit, matplotlib, seaborn')
 st.markdown('- Data source: https://docs.google.com/spreadsheets/d/1HoTUoAWX3jdS3WPz6chBwiEzj7BHD4uSd5Flx3bsbWE/edit?usp=sharing')
 
 df = pd.read_excel('Assignment Data Analyst MSIB Batch 7.xlsx')
+df_facebook = df[df['ga:sourceMedium'] == 'facebook / cpc']
+df_google = df[df['ga:sourceMedium'] == 'google / cpc']
 
 tab1, tab2, tab3 = st.tabs(["Peringkat Halaman", "Facebook", "Google"])
 
 with tab1:
   col1, col2 = st.columns(2)
   with col1:
-    st.table(data=df[:50])
+    st.table(data=df_facebook.groupby(by='ga:pageTitle').agg({
+    'ga:users': 'sum'
+}).sort_values(by='ga:users', ascending = False).head())
   with col2:
     st.table(data=df[:50])
